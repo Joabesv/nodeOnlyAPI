6b5e066cf7e7637c37b973ac10ee1e1d7373507f
Joabesv <joabevarjao123@gmail.com>
2022-05-27 13:18:33 -0300

    crianado a rota get

--- src/routes/hero.routes.js
+++ src/routes/hero.routes.js
@@ -3,12 +3,13 @@ import { Hero } from '../entities/hero.js';
 import { DEFAULT_HEADER } from '../util/index.js';
 const routes = ({ heroService }) => ({
   '/heroes:get': async (request, response) => {
-    response.write('GET');
+    const heroes = await heroService.find();
+    response.write(JSON.stringify({ heroes }));
     response.end();
   },
 
   '/heroes:post': async (request, response) => {
-    // espero ocorrer o evento para capturar o valor
+    // once -> espero ocorrer o evento para capturar o valor
     const data = await once(request, 'data');
     const item = JSON.parse(data);
     const hero = new Hero(item);
