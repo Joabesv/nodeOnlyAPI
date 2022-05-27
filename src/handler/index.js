1fe773b5625febd0f934d4088dceb9ced3d95a56
Joabesv <joabevarjao123@gmail.com>
2022-05-27 10:04:35 -0300

    making global error handler

--- src/handler/index.js
+++ src/handler/index.js
@@ -2,7 +2,7 @@ import { parse } from 'node:url';
 import { DEFAULT_HEADER } from '../util/index.js';
 
 const allRoutes = {
-  '/heroes:get': (request, response) => {
+  '/heroes:get': async (request, response) => {
     response.write('GET');
     response.end();
   },
@@ -15,11 +15,28 @@ const allRoutes = {
   },
 };
 
-export function handler(request, response) {
+function handler(request, response) {
   const { url, method } = request;
   const { pathname } = parse(url, true);
   const key = `${pathname}:${method.toLowerCase()}`;
   const chosen = allRoutes[key] || allRoutes.default;
 
-  return chosen(request, response);
+  return Promise.resolve(chosen(request, response)).catch(
+    handlerError(response)
+  );
 }
+
+function handlerError(response) {
+  return error => {
+    console.log(`Morreu o app jão`, error.stack);
+    response.writeHead(500, DEFAULT_HEADER);
+    response.write(
+      JSON.stringify({
+        error: 'internal server error',
+      })
+    );
+    return response.end();
+  };
+}
+
+export { handler };
